24224e1c4beb630e366eb663b66b7fc909736963
FranciscoCumini <cuminifrancisco@gmail.com>
2025-06-05 21:41:54 -0300

    feat: mejorar estilos y estructura en CardWidget, Layout y NotFound

--- src/components/CardWidget.jsx
+++ src/components/CardWidget.jsx
@@ -2,13 +2,20 @@ import { Button, Card } from 'react-bootstrap';
 
 const CardWidget = ({ card, onRoute }) => {
   return (
-    <Card className="text-center" style={{ width: '20rem', height: '20rem' }}>
+    <Card
+      className="text-center bg-dark text-white border-warning shadow-lg rounded-3"
+      style={{ width: '20rem', height: '20rem' }}
+    >
       <Card.Body className="d-flex flex-column justify-content-center align-items-center">
         <Card.Title>
           <h2>{card.title}</h2>
         </Card.Title>
         <Card.Text>{card.text}</Card.Text>
-        <Button onClick={() => onRoute(card.pathname)}>
+        <Button
+          variant="warning"
+          className="fw-medium"
+          onClick={() => onRoute(card.pathname)}
+        >
           {card.buttonName}
         </Button>
       </Card.Body>
